87758866d0596d776bad5b557eab05e1ef874e03
AndreyU <ukladnikovandrey@yandex.ru>
2019-07-20 15:32:22 +0300

    Д З к Урок 6. Вебинар «Транзакции, переменные, представления. Администрирование MySQL»

--- Ukladnikov_Andrey_HomeWork_Lesson6_additional.sql
+++ Ukladnikov_Andrey_HomeWork_Lesson6_additional.sql
@@ -18,4 +18,88 @@ GROUP BY user_id
 ORDER BY total DESC
 LIMIT 1;
 
+-- 2 Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей.
+-- Запросы необходимо строить с использованием JOIN !
+SELECT 
+	COUNT(*) as total_likes
+FROM 
+	likes lk INNER JOIN media md
+    ON lk.to_subject_id=md.id
+    INNER JOIN (SELECT youngest.user_id as yong_id FROM
+			(SELECT
+				user_id,
+				TIMESTAMPDIFF(DAY, birthday, NOW()) AS age_days
+			FROM 
+				profiles
+			ORDER BY age_days
+			LIMIT 10) as youngest) t
+    ON md.user_id=t.yong_id;
+	
+-- 3. Определить кто больше поставил лайков (всего) - мужчины или женщины?
+-- Запросы необходимо строить с использованием JOIN !
+SELECT 
+	tot_likes.sex as sex, 
+	tot_likes.total_likes as sum_likes
+FROM 
+	(
+    SELECT 
+		'f' as sex, 
+		f_likes.total as total_likes
+	FROM 
+		(SELECT count(*) as total
+		FROM likes lk INNER JOIN (SELECT user_id FROM profiles WHERE sex = 'f') t1
+		ON lk.from_user_id = t1.user_id				
+		) AS f_likes
+	UNION
+	SELECT 
+		'm' as sex, 
+		m_likes.total as total_likes
+	FROM 
+		(SELECT count(*) as total
+		FROM likes lk INNER JOIN (SELECT user_id FROM profiles WHERE sex = 'm') t2
+		ON lk.from_user_id=t2.user_id 	
+		) AS m_likes
+	  ) AS tot_likes
+ORDER BY sum_likes DESC 
+LIMIT 1;
+
+-- Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети.
+-- Скорее всего здесь если и будет запрос joinами, то неудачный
+SELECT 
+	users_activity.user_id AS user_id, 
+	SUM(users_activity.activity) total
+FROM 
+	(SELECT 
+		from_user_id user_id,
+		count(*) activity
+	FROM 
+		likes
+	GROUP BY
+		user_id
+
+	UNION ALL
+	
+	SELECT
+		from_user_id user_id,
+		count(*) activity
+	FROM
+		messages
+	GROUP BY 
+		user_id
+	
+	UNION ALL
+	
+	SELECT
+		to_user_id user_id,
+		count(*) activity
+	FROM
+		messages
+	GROUP BY 
+		user_id) users_activity
+
+GROUP BY 
+	user_id
+ORDER BY 
+	total
+LIMIT 10;
 
